7590b296e7a18a39598f703cc46e8271357c240f
xiyang <jbossbc@gmail.com>
2023-06-05 08:14:35 +0800

    fontend complete

--- code/fontend/src/components/Navbar.jsx
+++ code/fontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {HiMenuAlt4} from "react-icons/hi";
 import {AiOutlineClose}from "react-icons/ai";
 // import {LoginModal} from "../App.jsx";
 import logo from "../images/logo.png";
-import {Avatar,Button,Collapse,Descriptions,Drawer, Modal} from "antd";
+import {Avatar,Button,Progress,Descriptions,Drawer, Modal} from "antd";
 import { Web3Provider, tokenAddress } from "../App";
 import { BigNumber, ethers } from "ethers";
 import  baseERC20 from "../ABI/baseERC20ABI.json";
@@ -12,30 +12,47 @@ import  baseERC20 from "../ABI/baseERC20ABI.json";
 const Navbar =(props)=>{
     const addresses = useContext(tokenAddress);
     const ethProvider=useContext(Web3Provider);
-    const {balancesChange,setBalancesChange,isLogin,userInfo,setIsLogin,EOAInfo}=props;
+    const {isLogin,userInfo,setIsLogin,EOAInfo}=props;
+    const[balancesProgress,setBalancesProgress]=useState(0);
     // console.log(props);
     // const {userModalView} =props;
     const [toggleMenu,setToggleMenu]=React.useState(false);
     const [DrawerView,setDrawerView]=useState(false);
-    const [balancesView,setBalancesView]=useState(false);
+    const [loadingForTime,setLoadingForTime]=useState(null);
     // const setLoginView=useContext(LoginModal);
     // const [loginState,setLoginState]=useState(false);
-    const [balances,setBalances]=useState(new Map());
+    const [balances,setBalances]=useState(new Map(Array.from(addresses.keys()).map((key) => [key, null])));
     useEffect(async()=>{
-        if (balancesChange){
+        await loadingBalances();
+    },[])
+    async function loadingBalances(){
+        setBalancesProgress(0);
+        let walk =100/addresses.size;
+        try{
          addresses.forEach(async(value,key)=>{
             let balance =await getBalance(value);
-             setBalances((pre)=>(pre.set(key,balance)))
+             setBalances((pre)=>(pre.set(key,balance+key)))
+             setBalancesProgress((pre)=>(pre+walk));
         })
-             setBalancesView(true);
-
+    }catch(e){
+        Modal.error({title:"error",content:"获取余额失败"});
+        return
     }
-    },[balancesChange])
+    }
+    useEffect(()=>{
+        if(DrawerView){
+           setLoadingForTime(setInterval(async()=>{await loadingBalances()},5000));
+        }else{
+            if(loadingForTime!=null){
+                clearInterval(loadingForTime);
+                setLoadingForTime(null);
+            }
+        }
+    },[DrawerView])
     async function getBalance(src){
-        try{
         if (src == '0x0000000000000000000000000000000000000000'){
            let eth= await ethProvider.getBalance(EOAInfo.wallet.address);
-           return ethers.utils.formatEther(eth)+"ETH"
+           return ethers.utils.formatEther(eth)
         }
         let contract= new ethers.Contract(src,baseERC20,ethProvider);
         let tokenNumber=""; 
@@ -43,9 +60,6 @@ const Navbar =(props)=>{
             tokenNumber=data.toString();
         });
         return tokenNumber;
-    }catch(e){
-        Modal.warning({title:"warning",content:"获取"+key+"余额失败"});
-    }
     }
 
     return(
@@ -55,7 +69,7 @@ const Navbar =(props)=>{
                 <img src={logo} alt="logo" className="w-32 cursor-pointer"/>
             </div>
             {isLogin? 
-                <Avatar onClick={()=>{setDrawerView(true);setBalancesChange(true)}} style={{ backgroundColor: '#00a2ae', verticalAlign: 'middle' }} size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80 }}>
+                <Avatar onClick={()=>{setDrawerView(true)}} style={{ backgroundColor: '#00a2ae', verticalAlign: 'middle' }} size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80 }}>
         {userInfo.username}
       </Avatar>:<></>}
             <div className="flex relative">
@@ -70,14 +84,17 @@ const Navbar =(props)=>{
              </ul>)}
             </div>
         </nav>
-        <Drawer  open={DrawerView} onClose={()=>{setDrawerView(false)}}>
+        <Drawer style={{background:"",}}  open={DrawerView} onClose={()=>{setDrawerView(false)}}>
       
                 <Descriptions bordered size="small" column={1}>
                     <Descriptions.Item label="地址" selectable>
                        {EOAInfo.wallet!=null&&EOAInfo.wallet.address}
                     </Descriptions.Item>
-                            {isLogin&&balancesView&&balances.forEach((value,key)=><Descriptions.Item label={key}>{value}</Descriptions.Item>)}
+                            {isLogin&&Array.from(addresses.keys()).map((value,index)=><Descriptions.Item key={value} label={value}>{balances.get(value)}</Descriptions.Item>)}
                     <Descriptions.Item>
+                   <Descriptions.Item>
+                   <Progress percent={balancesProgress} size="small" />
+                   </Descriptions.Item>
                     <Button className=" w-full cursor-pointer" onClick={()=>{setIsLogin(false);setDrawerView(false)}}>退出登录</Button>
                     </Descriptions.Item>
                 </Descriptions>     
